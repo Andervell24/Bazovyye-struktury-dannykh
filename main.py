7845e22c9c4ced5b67ea0ba4e8d745d311ff40e4
Andervell24 <andervell24@gmail.com>
2024-09-06 05:43:02 +0300

    main.py
    Практическое задание по уроку "Базовые структуры данных"

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print (9.99 > 9.98 and 1000 != 1000.1)
 print("3rd program")
 print (2 * 2 + 2)
 print (2 * (2 + 2))
-print (6 == 8)
+print ((2 * 2 + 2) == (2 * (2 + 2)))
 print("4rd program")
 print(int((float('123.456')-int(float('123.456')))*10))
 
